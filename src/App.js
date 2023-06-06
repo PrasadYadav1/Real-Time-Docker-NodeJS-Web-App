fb8c4df6ad233354b54b38f9d6a08763ec6b1f28
karthik <prasadkarthikeyan@gmail.com>
2023-06-06 21:47:33 +0530

    Updated Values in app.js file

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ const App = () => {
       <Confetti />
       <header className="App-header">
         <h1 style={{ marginBottom: "0px" }}>
-          Congratulations!!!
+          Welcome to Docker World to our TI Gems!!!
         </h1>
         <p style={{ marginTop: "10px", marginBottom: "50px" }}>
-          You ran your first container.
+          Great to be a part of Technoidentity.
         </p>
         <div>
           <a target="_blank" href={"https://twitter.com/intent/tweet?text=" + shareMessage + "&url=" + shareLink} class="fa fa-twitter" rel="noopener noreferrer"> </a>
-          <a target="_blank" href={"https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink} class="fa fa-linkedin" rel="noopener noreferrer"> </a>
+          <a target="_blank" href={"https://www.linkedin.com/company/technoidentity/?originalSubdomain=in" + shareLink} class="fa fa-linkedin" rel="noopener noreferrer"> </a>
           <a target="_blank" href={"https://reddit.com/submit?title=" + shareMessage + "&url=" + shareLink} class="fa fa-reddit" rel="noopener noreferrer"> </a>
         </div>
       </header>
